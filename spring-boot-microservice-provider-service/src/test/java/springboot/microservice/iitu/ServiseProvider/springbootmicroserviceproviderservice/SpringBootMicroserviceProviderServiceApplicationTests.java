5922ebe0fdc71cd930adb78e9062c92170748b37
Madi Muratkhan <madi.muratkhan@gmail.com>
2021-09-26 20:56:47 +0600

    add servise provider

---
+++ spring-boot-microservice-provider-service/src/test/java/springboot/microservice/iitu/ServiseProvider/springbootmicroserviceproviderservice/SpringBootMicroserviceProviderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.microservice.iitu.ServiseProvider.springbootmicroserviceproviderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceProviderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
