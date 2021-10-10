c8647c000d5ee22459492903104183f36de59a11
Madiko <65715613+MadiMuratkhan@users.noreply.github.com>
2021-10-11 02:24:49 +0600

    Add files via upload

---
+++ spring-boot-microservice-provider-service/src/test/java/springboot/microservice/iitu/ServiseProvider/springbootmicroserviceproviderservice/service.java
@@ -0,0 +1,13 @@
+package springboot.microservice.iitu.ServiseProvider.springbootmicroserviceproviderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceProviderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceProviderServiceApplication.class, args);
+	}
+
+}
